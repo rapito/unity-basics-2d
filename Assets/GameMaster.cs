d0c289057a2d98a476d1d55379668fa70d4b275f
Robert Peralta <robert.apb@hotmail.com>
2015-02-28 01:23:42 -0400

    ReSpawning Player

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -3,9 +3,28 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 
+	public static GameMaster GM;
+	public Transform playerPrefab;
+	public Transform spawnPoint;
+
+
+	void Start()
+	{
+		if (GM == null) {
+			GM = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+		}
+	}
+
+	public void RespawnPlayer()
+	{
+		Transform player = Instantiate(playerPrefab,spawnPoint.position,spawnPoint.rotation);
+
+	}
+
 	public static void KillPlayer(Player player)
 	{
 		Destroy(player.gameObject);
+		GM.RespawnPlayer ();
 	}
 
 }
